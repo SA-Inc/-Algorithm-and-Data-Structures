30bbfd0f7057761fd2c29ca85829155d9aff9c77
Arman Saparov <s.a.inc.17@gmail.com>
2023-08-02 14:46:17 +0600

    Update binary.py

--- search/binary.py
+++ search/binary.py
@@ -1,3 +1,9 @@
+# 1. find middle index
+# 2. if mid index element == target, insert to mid
+# 3. if mid index element < target, move left index by 1
+# 3. if mid index element > target, move right index by 1
+# 4. return left index, because near to 0 index
+
 def binary_search(array, search):
   left_index = 0
   right_index = len(array) - 1
