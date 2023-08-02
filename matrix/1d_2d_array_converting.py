1c1f14e4b6307725a817cb0c363a307fa0b71f09
Arman Saparov <s.a.inc.17@gmail.com>
2023-08-02 16:32:49 +0600

    Update and rename 1d_to_2d_array.py to 1d_2d_array_converting.py

--- matrix/1d_2d_array_converting.py
+++ matrix/1d_2d_array_converting.py
@@ -14,3 +14,24 @@ def one_dim_to_two_dim_array(original: list[int], rows: int, cols: int) -> list[
 
 
 print(one_dim_to_two_dim_array(original = [1,2,3,4], rows = 2, cols = 2))
+
+
+def two_dim_to_one_dim_array(matrix):
+  rows = len(matrix)
+  cols = len(matrix[0])
+  array = [0 for i in range(rows * cols)]
+
+  for i in range(0, rows * cols, 1):
+    row = i // cols
+    col = i % cols
+    array[i] = matrix[row][col]
+
+  return array
+
+
+print(two_dim_to_one_dim_array([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [0, 0, 0]
+]))
