9b85ad1d18d603dc5190f56b4f1e309529c8a3f3
Arman Saparov <s.a.inc.17@gmail.com>
2023-08-03 15:18:01 +0600

    Update odd_even.py

--- sorting/odd_even.py
+++ sorting/odd_even.py
@@ -1,3 +1,30 @@
+# looks like comb
+# 1. check if not sorted in two inner loops, perform swap
+# 2. check flag, if not sorted repeat inner loops
+
+def even_odd_sort(array):
+  is_sorted = False
+
+  while(is_sorted == False):
+    is_sorted = True
+
+    # compare 0, 2, 4, ... indexes - even
+    for i in range(0, len(array) - 1, 2):
+      if(array[i] > array[i + 1]):
+        temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+        is_sorted = False
+
+    # compare 1, 3, 5, ... indexes - odd
+    for i in range(1, len(array) - 2, 2):
+      if(array[i] > array[i + 1]):
+        temp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+        is_sorted = False
+
+
 # on each iteration compare even elements and odd elements
 # if visualise it is look like brick wall
 
