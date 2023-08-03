51e338d1932329800a7c3b13d83a9b8c72cc664e
Arman Saparov <s.a.inc.17@gmail.com>
2023-08-03 20:35:01 +0600

    Update merge.py

--- array/merge.py
+++ array/merge.py
@@ -26,3 +26,45 @@ a = [1, 2, 3]
 b = [4, 5, 6]
 
 print(array_merge(a, b))
+
+
+
+# merge different size arrays
+
+def merge(num1, num2):
+  arr3 = [None]*(len(num1)+len(num2))
+  i, j, k = 0, 0, 0
+
+  while i < len(arr1) and j < len(arr2):
+    if arr1[i] < arr2[j]:
+      arr3[k] = arr1[i]
+      k += 1
+      i += 1
+    else:
+      arr3[k] = arr2[j]
+      k += 1
+      j += 1
+
+  while i < len(num1):
+    arr3[k] = arr1[i];
+    k += 1
+    i += 1
+
+  while j < len(num2):
+    arr3[k] = arr2[j];
+    k += 1
+    j += 1
+
+  return arr3
+
+arr1 = [1, 3]
+arr2 = [2]
+print(merge(arr1, arr2))
+
+arr1 = [1, 3, 4, 5]
+arr2 = [2, 4, 8]
+print(merge(arr1, arr2))
+
+arr1 = [5, 9]
+arr2 = [4, 7, 8]
+print(merge(arr1, arr2))
