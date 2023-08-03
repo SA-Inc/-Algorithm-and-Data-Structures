5455738d7025a4111970ce1c899d9b81287dd14a
Arman Saparov <s.a.inc.17@gmail.com>
2023-08-03 20:24:34 +0600

    Create amp.py

---
+++ string/amp.py
@@ -0,0 +1,48 @@
+def get_lps(s):
+  # LPS - Longest Proper Prefix
+  lps = [0] * len(s)
+  i = 1
+  j = 0
+
+  while i < len(s):
+    if(s[i] == s[j]):
+      j += 1
+      lps[i] = j
+      i += 1
+    elif(j > 0):
+      j = lps[j - 1]
+    else:
+      lps[i] = 0
+      i += 1
+  return lps
+
+
+def kmp(s1, s2):
+  n = len(s1)
+  m = len(s2)
+
+  if(m > n):
+    return -1
+  if(m == n):
+    return 0 if s2 == s1 else -1
+  if(s2 == ""):
+    return 0
+
+  lps = get_lps(s2)
+
+  i = 0
+  j = 0
+
+  while(i < n and j < m):
+    if(s1[i] == s2[j]):
+      i += 1
+      j += 1
+    elif(j > 0):
+      j = lps[j - 1]
+    else:
+      i += 1
+  return -1 if j < m else i-m
+
+
+# print(get_lps('aadfgha'))
+print(kmp('aaaa', 'aa'))
