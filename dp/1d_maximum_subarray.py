4bd209b1fe82458e9d7bd00fbf9c931eb775003d
Arman Saparov <s.a.inc.17@gmail.com>
2023-12-06 14:35:11 +0600

    Update 1d_maximum_subarray.py

--- dp/1d_maximum_subarray.py
+++ dp/1d_maximum_subarray.py
@@ -3,15 +3,29 @@ def kadane(array):
   current_sum = 0
   max_sum = -999999
 
+  # start, end subarray index
+  left = 0
+  right = 0
+  # tmp offset for start index
+  s = 0
+
   # on each step get current and max sum from 0 to i
   # reset current sum if negative
   for i in range(0, n, 1):
     current_sum += array[i]
-    max_sum = max(max_sum, current_sum)
+
+    # move left and right
+    if(max_sum < current_sum):
+      max_sum = current_sum
+      left = s
+      right = i
+
+    # move tmp offset to right
     if(current_sum < 0):
       current_sum = 0
+      s = i + 1
 
-  return max_sum
+  return (left, right, max_sum)
 
 
 a = [-1, 2, -2, 5, 7, -3, 1]
