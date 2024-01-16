53a89896c234530263a273cb5b359ec97f16b7d4
Arman Saparov <s.a.inc.17@gmail.com>
2024-01-16 14:53:05 +0600

    Update simple_perceptron.py

--- neural_network/simple_perceptron.py
+++ neural_network/simple_perceptron.py
@@ -1,38 +1,62 @@
-weights = [0.2, -0.1]
-bias = -0.29
-learning_rate = 0.01
+import numpy as np
 
 
-def learn(input_data, label):
-  for i in range(0, len(input_data), 1):
-    # print(input_data[i], label[i])
-
-    # w * i
-    input_weights_sum = 0
-    for w in range(0, len(weights), 1):
-      input_weights_sum += input_data[i][w] * weights[w]
-
-    input_weights_sum += bias
-    predict = 1 if input_weights_sum > 0 else 0
-    error = label[i] - predict
-
-    # print(input_weights_sum, predict, error)
-
-    for w in range(0, len(weights), 1):
-      weights[w] += learning_rate * error * input_data[i][w]
+def activation(x):
+  return 0 if x < 0.5 else 1
 
 
-learn_input_data = [
-  [1, 1],
-  [2, 2],
-  [4, 4],
-  [5, 5]
-]
+def perceptron(input_data):
+  # weights of each neuron on the same layer
+  hidden_1_1 = [0.3, 0.3, 0]
+  hidden_1_2 = [0.4, -0.5, 1]
 
-learn_output_data = [0, 0, 1, 1]
+  # 2x3
+  weights_1 = [hidden_1_1, hidden_1_2]
+  # 1x2
+  weights_2 = [-1, 1]
 
-predict_input_data = [5, 4]
+  # dot product weights and inputs
+  # s = w * i
+  # np.dot() function
+  weights_sum = []
+  for i in range(0, len(weights_1), 1):
+    weights_layer_sum = 0
+    for d in range(0, len(input_data), 1):
+      weights_layer_sum += weights_1[i][d] * input_data[d]
+    weights_sum.append(weights_layer_sum)
+
+  # apply activation function for each weight sum
+  out_hidden = []
+  for i in range(0, len(weights_sum), 1):
+    out_hidden.append(activation(weights_sum[i]))
+
+  # dot product weights and output hidden
+  weights_sum = 0
+  for i in range(0, len(weights_2), 1):
+    weights_sum += weights_2[i] * out_hidden[i]
+
+  output_data = activation(weights_sum)
+  return output_data
 
 
-learn(learn_input_data, learn_output_data)
-print(weights)
+  # # numpy alternative
+  # input_data = np.array(input_data)
+  # sum_hidden = np.dot(weights_1, input_data)
+  # out_hidden = np.array([activation(x) for x in sum_hidden])
+  # sum_end = np.dot(weights_2, out_hidden)
+  # output_data = activation(sum_end)
+  # print(output_data)
+
+
+have_house = 1
+have_money = 0
+have_phone = 1
+
+input_data = [have_house, have_money, have_phone]
+
+output_data = perceptron(input_data)
+
+if(output_data == 1):
+  print('ok')
+else:
+  print('no')
